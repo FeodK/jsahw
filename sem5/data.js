e74903fe43e11084ffc9c1d1766ae3a2091d2f85
Feodor <mr.fedor.kiselev@gmail.com>
2024-09-05 21:06:22 +0300

    sem 5

---
+++ sem5/data.js
@@ -0,0 +1,65 @@
+// https://api.thecatapi.com/v1/images/search?limit=10
+
+const dataInfo = `
+[
+  {
+    "id": "1ts",
+    "url": "https://cdn2.thecatapi.com/images/1ts.jpg",
+    "width": 640,
+    "height": 960
+  },
+  {
+    "id": "6p3",
+    "url": "https://cdn2.thecatapi.com/images/6p3.gif",
+    "width": 245,
+    "height": 188
+  },
+  {
+    "id": "99r",
+    "url": "https://cdn2.thecatapi.com/images/99r.jpg",
+    "width": 560,
+    "height": 473
+  },
+  {
+    "id": "a40",
+    "url": "https://cdn2.thecatapi.com/images/a40.jpg",
+    "width": 500,
+    "height": 739
+  },
+  {
+    "id": "b47",
+    "url": "https://cdn2.thecatapi.com/images/b47.jpg",
+    "width": 900,
+    "height": 601
+  },
+  {
+    "id": "bmo",
+    "url": "https://cdn2.thecatapi.com/images/bmo.jpg",
+    "width": 720,
+    "height": 540
+  },
+  {
+    "id": "btd",
+    "url": "https://cdn2.thecatapi.com/images/btd.jpg",
+    "width": 800,
+    "height": 516
+  },
+  {
+    "id": "cfm",
+    "url": "https://cdn2.thecatapi.com/images/cfm.jpg",
+    "width": 1024,
+    "height": 768
+  },
+  {
+    "id": "MTYzMjQyNA",
+    "url": "https://cdn2.thecatapi.com/images/MTYzMjQyNA.jpg",
+    "width": 612,
+    "height": 612
+  },
+  {
+    "id": "wwFp9IiRd",
+    "url": "https://cdn2.thecatapi.com/images/wwFp9IiRd.jpg",
+    "width": 1214,
+    "height": 826
+  }
+]`;
